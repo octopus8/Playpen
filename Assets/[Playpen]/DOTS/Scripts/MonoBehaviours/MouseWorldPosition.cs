bcf9ec0b70dde1a99e3b57ab527c073d64903876
Brian A. Lee <octopus8creations@gmail.com>
2025-08-17 20:19:07 -1000

    Using physics to get MouseWorldPosition.

--- Assets/[Playpen]/DOTS/Scripts/MonoBehaviours/MouseWorldPosition.cs
+++ Assets/[Playpen]/DOTS/Scripts/MonoBehaviours/MouseWorldPosition.cs
@@ -13,11 +13,18 @@ public class MouseWorldPosition : MonoBehaviour
     public Vector3 GetMouseWorldPosition()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            return hit.point;
+        }
+/*        
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
         if (groundPlane.Raycast(ray, out float enter))
         {
             return ray.GetPoint(enter);
         }
         return Vector3.zero; // Return zero if the ray does not hit the ground plane
+*/    
     }
 }
