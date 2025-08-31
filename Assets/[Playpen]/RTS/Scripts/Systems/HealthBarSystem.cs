ec4fe63450ee89485216605160badc6bf6ce71ed
Brian A. Lee <octopus8creations@gmail.com>
2025-08-31 13:03:47 -1000

    Health bar faces camera.

--- Assets/[Playpen]/RTS/Scripts/Systems/HealthBarSystem.cs
+++ Assets/[Playpen]/RTS/Scripts/Systems/HealthBarSystem.cs
@@ -2,6 +2,7 @@ using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
 
 namespace RTS
 {
@@ -11,11 +12,20 @@ partial struct HealthBarSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var healthBar in
+        Vector3 cameraForward = Vector3.zero;
+        if (Camera.main != null)
+        {
+            cameraForward = Camera.main.transform.forward;
+        }
+        foreach (var (localTransform, healthBar) in
                  SystemAPI.Query<
-                     RefRW<HealthBar>
+                     RefRW<LocalTransform>,
+                     RefRO<HealthBar>
                  >())
         {
+            LocalTransform parentLocalTransform = SystemAPI.GetComponent<LocalTransform>(healthBar.ValueRO.healthEntity);
+            localTransform.ValueRW.Rotation = parentLocalTransform.InverseTransformRotation(quaternion.LookRotation(cameraForward, math.up()));
+            
             Health health = SystemAPI.GetComponent<Health>(healthBar.ValueRO.healthEntity);
             float healthNormalized = (float)health.currentHealth / health.maxHealth;
 
