9db8274c632f6d4c3bd535948fc2b072c03a7323
Brian A. Lee <octopus8creations@gmail.com>
2025-09-09 19:38:54 -1000

    Removed zombie none animation.

--- Assets/[Playpen]/RTS/ScriptableObjects/FlipbookAnimationScriptableObject.cs
+++ Assets/[Playpen]/RTS/ScriptableObjects/FlipbookAnimationScriptableObject.cs
@@ -9,7 +9,6 @@ public class FlipbookAnimationScriptableObject : ScriptableObject
         SoldierNone,
         SoldierIdle,
         SoldierWalk,
-        ZombieNone,
         ZombieIdle,
         ZombieWalk
     }
